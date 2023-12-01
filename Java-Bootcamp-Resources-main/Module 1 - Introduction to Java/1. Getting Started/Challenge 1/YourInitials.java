ef35c10fb4d4faeb93a6f7a00d7d418247ee78a9
andrewbantly <andrewbantly@gmail.com>
2023-12-01 06:39:14 -0800

    start of java course

---
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
@@ -0,0 +1,11 @@
+public class YourInitials {
+    public static void main(String[] args) {
+        System.out.println("  A    BBBBB");
+        System.out.println(" A A   B   B");
+        System.out.println("A   A  B   B");
+        System.out.println("AAAAA  BBBBB");
+        System.out.println("A   A  B   B");
+        System.out.println("A   A  B   B");
+        System.out.println("A   A  BBBBB");
+    }
+}
---
+++ sect1/HelloJava.java
@@ -0,0 +1,16 @@
+// Java uses Camel Case, where the first letter of each word is capitalized
+
+// The Class name needs to always match the file name it is in 
+
+/**
+ * HelloJava
+ */
+public class HelloJava {
+    // Main method is the entry point of a java application - where java starts running your code
+    public static void main(String[] args) {
+        // println prints a message to the console
+        // the x: is an inlay hint
+            // Inlay hints are special markers that appear in the editor and provide you with additional information about your code    
+        System.out.println("Hello, Java");
+    }
+}
